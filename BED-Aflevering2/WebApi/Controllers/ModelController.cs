0475d11797f5d3dc911b828e0d9433f3f9b3eab5
au682545 <tigriss.tiger@gmail.com>
2023-03-14 10:59:39 +0100

    .

---
+++ BED-Aflevering2/WebApi/Controllers/ModelController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ModelController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+        public ModelController(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/Models
+        [HttpGet]
+        public List<ModelDto> GetModels()
+        {
+            var models = from m in _context.Models
+                         select m;
+            List<ModelDto> modelDtos = new List<ModelDto>();
+            foreach (Model m in models)
+            {
+                ModelDto mDto = _mapper.Map<ModelDto>(m);
+                modelDtos.Add(mDto);
+            }
+            return modelDtos;
+        }
+
+        // GET: api/Models/5
+        [HttpGet]
+        public async Task<ActionResult<ModelDtoFull>> GetModel(long id)
+        {
+            var model = await _context.Models.FindAsync(id);
+            if (model == null)
+            {
+                return NotFound("Model could not be found");
+            }
+
+            _context.Entry(model)
+                .Collection(m => m.Jobs)
+                .Load();
+
+            ModelDtoFull retModel = _mapper.Map<ModelDtoFull>(model);
+
+            if (model.Jobs != null)
+            {
+                retModel.Jobs = new List<JobDtoSimple>();
+                foreach (Job j in model.Jobs)
+                {
+                    JobDtoSimple job = _mapper.Map<JobDtoSimple>(j);
+                    retModel.Jobs.Add(job);
+                }
+            }
+
+            if (model.Expenses != null)
+            {
+                retModel.Expenses = new List<ExpenseDto>();
+                foreach (Expense e in model.Expenses)
+                {
+                    ExpenseDto expense = _mapper.Map<ExpenseDto>(e);
+                    retModel.Expenses.Add(expense);
+                }
+            }
+            return retModel;
+        }
+    }
+}
---
+++ BED-Aflevering2/WebApi/Models/ModelDto.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApi.Models
+{
+    public class ModelDto
+    {
+        public long ModelId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNo { get; set; }
+
+        public string? AddresLine1 { get; set; }
+        public string? AddresLine2 { get; set; }
+
+        public string? Zip { get; set; }
+
+        public string? City { get; set; }
+
+        public DateTime BirthDay { get; set; }
+        public double Height { get; set; }
+        public int ShoeSize { get; set; }
+        public string? HairColor { get; set; }
+        public string? Comments { get; set; }
+
+    }
+}
