77cfbe2b7a10a4af0d84b7d7e2c68c9f16106ec2
AlanKhamo <alankhamo@gmail.com>
2023-03-14 10:01:56 +0100

    adding packages and TodoDb

--- BED-Aflevering2/WebApi/Data/TodoDb.cs
+++ BED-Aflevering2/WebApi/Data/TodoDb.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using WebApi.Models;
 
 
 namespace WebApi.Data
 {
-    public class TodoDb
+    public class TodoDb :DbContext
     {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
 
-       
+        public DbSet<Model> model => Set<Model>();
+        public DbSet<Job> job => Set<Job>();
+
+        public DbSet<Expense> expense => Set<Expense>();
+
     }
 }
