e26bb0ee46f68f7a49b5a26b57ac3e3ae29b2f12
AlanKhamo <alankhamo@gmail.com>
2023-03-14 10:46:39 +0100

    add JobController

---
+++ BED-Aflevering2/WebApi/Controllers/JobController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using WebApi.Data;
+using WebApi.Hubs;
+using AutoMapper;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase 
+    {
+        private readonly DataContext context_;
+        private readonly IMapper mapper_;
+
+        public JobController(DataContext context, IMapper mapper)
+        {
+            context_= context;
+            mapper_ = mapper;
+        }
+       
+    }
+}
