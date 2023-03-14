dd5a894cfbb103125599adf33951835d598bb9ef
au682545 <tigriss.tiger@gmail.com>
2023-03-14 14:59:46 +0100

    .

---
+++ BED-Aflevering2/WebApi/Startup.cs
@@ -0,0 +1,20 @@
+﻿using WebApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+        }
+    }
+}
